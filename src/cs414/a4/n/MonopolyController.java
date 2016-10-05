9469b0052ca211ac1e3ffd65416a0558bbde7047
Joey <josplode@gmail.com>
2016-10-05 15:42:01 -0600

    Class for testing web service

---
+++ src/cs414/a4/n/MonopolyController.java
@@ -0,0 +1,21 @@
+package cs414.a4.n;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class MonopolyController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MonopolyController.class, args);
+    }
+}
