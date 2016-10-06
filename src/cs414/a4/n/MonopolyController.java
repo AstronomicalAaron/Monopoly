e907ca62e016eac83123cfd0b79006e76abb758a
Joey <josplode@gmail.com>
2016-10-05 21:28:07 -0600

    Basic webserver and graphics

--- src/cs414/a4/n/MonopolyController.java
+++ src/cs414/a4/n/MonopolyController.java
@@ -2,17 +2,24 @@ package cs414.a4.n;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @EnableAutoConfiguration
 public class MonopolyController {
-
-    @RequestMapping("/")
+    
+	@RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
-    String home() {
-        return "Hello World!";
+    public FileSystemResource get() {
+		return getHttp("page.html");
+    }
+	
+    @RequestMapping(value = "/{file_name:.+}", method = RequestMethod.GET)
+    @ResponseBody
+    public FileSystemResource getHttp(@PathVariable("file_name") String fileName) {
+		return new FileSystemResource("http/" + fileName);
     }
 
     public static void main(String[] args) throws Exception {
