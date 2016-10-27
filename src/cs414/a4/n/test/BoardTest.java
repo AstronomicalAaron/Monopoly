9dd4c8ce9d36f5b3a94d1673db4a3d32a994f988
Chris Geohring <chrisgehg@gmail.com>
2016-10-27 17:08:47 -0600

    Updated Tests

--- src/cs414/a4/n/test/BoardTest.java
+++ src/cs414/a4/n/test/BoardTest.java
@@ -2,13 +2,43 @@ package cs414.a4.n.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import cs414.a4.n.Board;
+import cs414.a4.n.Die;
+import cs414.a4.n.TileList;
+import cs414.a4.n.TileType;
+
 public class BoardTest {
+	Board b;
+	@Before
+	public void setUp() throws Exception {
+		b = new Board();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testBoard() {
+		assertNotNull(b);
+	}
+
+	@Test
+	public void testGetTiles() {
+		TileList t = b.getTiles();
+		assertEquals(t.get(0).getType(), TileType.GO);
+	}
+
+	@Test
+	public void testGetDice() {
+		assertNotNull(b.getDice());
+		int rollValue = b.getDice()[0].roll();
+		assert(rollValue >= 0 && rollValue<= 6);
+		
+	}
+
+	@Test
+	public void testGetGo() {
+		assertEquals(b.getGo(), b.getTiles().get(0));
 	}
 
 }
--- src/cs414/a4/n/test/TokenTest.java
+++ src/cs414/a4/n/test/TokenTest.java
@@ -2,13 +2,48 @@ package cs414.a4.n.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import cs414.a4.n.Token;
+import cs414.a4.n.TokenType;
+
 public class TokenTest {
+	Token t;
+	@Before
+	public void setUp() throws Exception {
+		t = new Token(TokenType.DOG);
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testToken() {
+		assertNotNull(t);
+	}
+
+	@Test
+	public void testGetType() {
+		assertEquals(t.getType(), TokenType.DOG);
+	}
+
+	@Test
+	public void testGetTileIndex() {
+		//When created, a token is not set to any tile
+		assertNull(t.getTileIndex());
+	}
+
+	@Test
+	public void testMoveTo() {
+		assertNull(t.getTileIndex());
+		t.moveTo(2);
+		assertEquals(t.getTileIndex(), 2);
+	}
+
+	@Test
+	public void testMoveBy() {
+		//Tile is on 2, based off of past test
+		assertEquals(t.getTileIndex(), 2);
+		t.moveBy(1);
+		assertEquals(t.getTileIndex(), 3);
 	}
 
 }
