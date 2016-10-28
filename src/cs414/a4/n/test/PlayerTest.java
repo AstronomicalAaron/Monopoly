08acb0f95631552acb2c25aa6a8c1e3dde89a547
Chris Geohring <chrisgehg@gmail.com>
2016-10-27 18:52:08 -0600

    Added player tests

--- src/cs414/a4/n/test/PlayerTest.java
+++ src/cs414/a4/n/test/PlayerTest.java
@@ -10,18 +10,68 @@ import cs414.a4.n.TokenType;
 
 public class PlayerTest {
 
-	Player Player;
+	Player player;
 	
 	@Before
 	public void setUp() throws Exception {
-		Player = new Player("Frodo",TokenType.DOG);
+		player = new Player("Frodo",TokenType.DOG);
 	}
 
 	@Test
 	public void testPlayer() {
-		assert(Player.getMoney() == 1500);
-		assert(Player.getName().equals("Frodo"));
-		assert(Player.getToken().getType() == TokenType.DOG);
+		assert(player.getMoney() == 1500);
+		assert(player.getName().equals("Frodo"));
+		assert(player.getToken().getType() == TokenType.DOG);
 	}
 
+	@Test
+	public void testGetName() {
+		assertEquals(player.getName(), "Frodo");
+	}
+
+	@Test
+	public void testGetMoney() {
+		assertTrue(player.getMoney() == 1500);
+	}
+
+	@Test
+	public void testGetDeeds() {
+		assert(player.getDeeds().size() == 0);
+	}
+
+	@Test
+	public void testHasDeed() {
+		//Player does not own tile 1 // Medit ave...
+		assert(player.hasDeed(1) == false);
+	}
+
+	@Test
+	public void testTransfer() {
+		Player player2 = new Player("Sam", TokenType.HAT);
+		assert(player.getMoney() == 1500);
+		assert(player.transfer(player2, 100.0) == 1400);
+		assert(player.getMoney() == 1400);
+	}
+
+	@Test
+	public void testGetNumRailRoadsOwned() {
+		assert(player.getNumRailRoadsOwned() == 0);
+	}
+
+	@Test
+	public void testGetNumUtilitiesOwned() {
+		assert(player.getNumUtilitiesOwned() == 0);
+	}
+
+	@Test
+	public void testSetUtilitiesOwned() {
+		player.setUtilitiesOwned(1);
+		assert(player.getNumUtilitiesOwned() == 1);
+	}
+
+	@Test
+	public void testSetNumRailRoadsOwned() {
+		player.setNumRailRoadsOwned(1);
+		assert(player.getNumRailRoadsOwned() == 1);
+	}
 }
